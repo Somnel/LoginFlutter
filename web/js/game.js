1f583f7b87b9ef12006ab4682e4c5c88db12c638
Somnel <nelson.cl.machado@gmail.com>
2024-10-08 11:51:50 -0300

    update index

--- web/js/game.js
+++ web/js/game.js
@@ -183,3 +183,24 @@ $('#novoJogo').click(function(){
     newGame();
 })
 
+const carouselSlide = document.querySelector('.carousel-slide');
+const images = document.querySelectorAll('.carousel-slide img');
+
+let counter = 0;
+const size = images[0].clientWidth;
+
+document.querySelector('.next').addEventListener('click', () => {
+    if (counter >= images.length - 1) return;
+    carouselSlide.style.transition = 'transform 0.5s ease-in-out';
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    if (counter <= 0) return;
+    carouselSlide.style.transition = 'transform 0.5s ease-in-out';
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+
